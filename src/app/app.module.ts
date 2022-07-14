94b884a6b41643758bcde49dd6b3e0269e595e9e
albinaAS93 <102250096+albinaAS93@users.noreply.github.com>
2022-07-14 22:01:18 +0200

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,18 @@ import { ContentComponent } from './components/content/content.component';
 import { OffersComponent } from './components/offers/offers.component';
 import { JobDescriptionComponent } from './components/job-description/job-description.component';
 import { CompanyDetailsComponent } from './components/company-details/company-details.component';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqKCl7l2Geo8XKz4QCObqASveAFQXpX2g",
+  authDomain: "progetto-angular-1.firebaseapp.com",
+  projectId: "progetto-angular-1",
+  storageBucket: "progetto-angular-1.appspot.com",
+  messagingSenderId: "162085607655",
+  appId: "1:162085607655:web:7ae3620b2787cd3762aa42"
+};
+
+const app = initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [
